cd13e7b32a5106f5d33e3b1152dc271a384174be
matthewpateman <matthewpateman@me.com>
2013-05-27 18:22:59 +0100

    fixes issue with helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < ActionController::Base
   end
 
     def email_or_username?(content)
-
     # The first element in the array is the username, the 2nd element is the title of the link
     # All other elements will be ignored.
     if content =~ /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]+\z/i 
@@ -34,7 +33,7 @@ class ApplicationController < ActionController::Base
 
   def does_username_exist?(username)
     @user = User.find_by_username(username)
-    if @user.username = username 
+    if @user
       return true
     else
       return false
@@ -42,8 +41,9 @@ class ApplicationController < ActionController::Base
   end
 
   def does_email_exist?(email)
+    logger.info(email)
     @user = User.find_by_email(email)
-    if @user.email = email 
+    if @user
       return true
     else
       return false
@@ -51,7 +51,7 @@ class ApplicationController < ActionController::Base
   end
 
 
-helper_method :current_user, :logged_in?, :require_user
+helper_method :current_user, :logged_in?, :require_user, :does_email_exist?, :does_username_exist?
 
 
 end

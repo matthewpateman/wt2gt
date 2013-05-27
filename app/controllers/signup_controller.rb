403e799707856dd6808d3835f2b2c776aa636bb2
matthewpateman <matthewpateman@me.com>
2013-05-27 11:28:13 +0100

    checks if email exists

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -12,17 +12,29 @@ class SignupController < ApplicationController
     @password = params[:password]
     @password_confirmation = params[:password_confirmation]
 
+    # Check if entered email is in use already
+    if !@email.blank? then
+      @user = User.find_by_email(@email)
+      if @user.email = @email 
+        logger.info('SIGN UP - Email address is in use already!')
+        @incomplete = 2
+      end
 
-    if @email.blank? || @first_name.blank?  || @last_name.blank? then
-      logger.info('***** Some fields missing *****')
+    # Checks if fields from previous screen are entered.  
+    elsif @email.blank? || @first_name.blank?  || @last_name.blank? then
+      logger.info('SIGN UP - Some fields are missing')
       @incomplete = 1
+
+    #All info from previous screen is there  
     else
-      logger.info('***** All info there *****')
+      logger.info('SIGN UP - All info is there')
       @incomplete = 0
 
+      # Checks if the Step 1 fields are entered
+     
       if @username.blank? || @password.blank?   || @password_confirmation.blank?       
-        logger.info('******** A FEW FIELDS BLANK ********')   
-      else
+        logger.info('SIGN UP S1 - A FEW FIELDS BLANK')   
+        else
         logger.info('******** NO FIELDS MISSING ********') 
         if @password == @password_confirmation then 
           logger.info('******** PASSWORDS MATCH ********')
@@ -41,11 +53,8 @@ class SignupController < ApplicationController
           logging_in_user = User.find_by_username(@username)
           session[:user_id] = logging_in_user.id
           redirect_to :action => "step2"
-
-
         end
-      end
-    
+      end  
   end
 
   def step2

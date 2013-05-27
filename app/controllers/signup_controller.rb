0f67b6d6752ede2971d41341b04e7092be291932
matthewpateman <matthewpateman@me.com>
2013-05-27 10:45:07 +0100

    Updated Sign Up Process

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,5 +1,6 @@
 class SignupController < ApplicationController
   def index
+
   end
 
   def step1
@@ -7,42 +8,49 @@ class SignupController < ApplicationController
     @first_name = params[:first_name]
     @last_name = params[:last_name]
     @email = params[:email]
+    @username = params[:username]
+    @password = params[:password]
+    @password_confirmation = params[:password_confirmation]
 
-    if !@first_name.blank? || !@last_name.blank? || !@email.blank? then
-      logger.info('******** NO FIELDS BLANK ********')  
+
+    if @email.blank? || @first_name.blank?  || @last_name.blank? then
+      logger.info('***** Some fields missing *****')
+      @incomplete = 1
     else
-     @incomplete = 1
-    end
+      logger.info('***** All info there *****')
+      @incomplete = 0
 
+      if @username.blank? || @password.blank?   || @password_confirmation.blank?       
+        logger.info('******** A FEW FIELDS BLANK ********')   
+      else
+        logger.info('******** NO FIELDS MISSING ********') 
+        if @password == @password_confirmation then 
+          logger.info('******** PASSWORDS MATCH ********')
+          @user = User.new do |u|
+            u.first_name = @first_name
+            u.last_name = @last_name
+            u.email = @email
+            u.username = @username
+            u.password = @password
+            logger.info('******** USER CREATED ********')
+          end 
+          @user.save
+          logger.info('******** USER SAVED ********')
+
+          #Logging in user
+          logging_in_user = User.find_by_username(@username)
+          session[:user_id] = logging_in_user.id
+          redirect_to :action => "step2"
+
+
+        end
+      end
+    
   end
 
   def step2
-    @username = params[:username]
-    @email = params[:email]
-    @password = params[:password]
-    @password_confirmation = params[:password_confirmation] 
-    @first_name = params[:first_name]
-    @last_name = params[:last_name]
+  end
 
-    if !@username.blank? || !@email.blank? || !@password.blank? || !@password_confirmation.blank? then
-    logger.info('******** NO FIELDS BLANK ********')  
-      if @password == @password_confirmation then 
-        logger.info('******** PASSWORDS MATCH ********')
-        @user = User.new do |u|
-          u.first_name = @first_name
-          u.last_name = @last_name
-          u.email = @email
-          u.username = @username
-          u.password = @password
-          logger.info('******** USER CREATED ********')
-        end 
-        @user.save
-        logger.info('******** USER SAVED ********')
-        #Logging in user
-        logging_in_user = User.find_by_username(@username)
-        redirect_to :action => "step3"
-      end
-    end
 
   end
 
